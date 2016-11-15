cc10be2cb497d2d84aab061401edc64b0e551350
Max Aldunate <maxaldunate@gmail.com>
2016-11-15 23:22:21 +0100

    sample added and cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
+## How to use
+#
+# > my_matrix <- getInvertibleMatrix()
+# > special_matrix <- makeCacheMatrix(my_matrix)
+# > cacheSolve(special_matrix)
+#      [,1] [,2]
+# [1,]   -1  1.5
+# [2,]    1 -1.0
+# > cacheSolve(special_matrix)
+# getting cached data
+#      [,1] [,2]
+# [1,]   -1  1.5
+# [2,]    1 -1.0
+# >
+
+getInvertibleMatrix <- function(){
+	matrix(c(2,2,3,2),2,2)
+}
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
---
+++ cachevector_example.R
@@ -0,0 +1,37 @@
+## Sample functions
+
+## How to use
+#
+# > my_vector <- getBigVector()
+# > special_vector <- makeVector(my_vector)
+# > cachemean(special_vector)
+
+getBigVector <- function() {
+	1:999888888
+}
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
